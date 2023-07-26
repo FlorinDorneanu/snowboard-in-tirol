aafabd4d0880de898a0d58678d82c456f1b991b4
FlorinDorneanu <>
2023-07-26 20:35:12 +0000

    Delete function get_homepage

--- snowboard_blog/views.py
+++ snowboard_blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -10,5 +10,21 @@ class PostList(generic.ListView):
     paginate_by = 4
 
 
-def get_homepage(request):
-    return render(request, 'index.html')
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exist():
+            liked = True
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
