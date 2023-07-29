49026b0a4c2b4976c2f350f9bf73094d79ed2f52
FlorinDorneanu <>
2023-07-29 10:52:43 +0000

    Added file comments and parent_comment field to the CommentForm for supporting nested comments

--- snowboard_blog/forms.py
+++ snowboard_blog/forms.py
@@ -1,3 +1,11 @@
+'''
+forms.py
+
+This module contains Django forms used in the application.
+
+- CommentForm: A form used for submitting comments, including support for nested comments.
+'''
+
 from .models import Comment
 from django import forms
 
@@ -5,4 +13,4 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ('body', 'parent_comment')
