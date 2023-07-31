5ba76a7883d1a56a5a4a8c73a392f5637279f1b7
FlorinDorneanu <>
2023-07-31 22:47:41 +0000

    Add like comment url

--- snowboard_blog/urls.py
+++ snowboard_blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('comment/<int:comment_id>/like/',
+         views.CommentLike.as_view(), name='comment_like'),
 ]
