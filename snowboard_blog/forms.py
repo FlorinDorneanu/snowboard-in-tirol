11d62e1979098b9eabf1abf5b4e080ad38369c2a
FlorinDorneanu <>
2023-08-07 23:36:06 +0000

    remove label and add widget for body

--- snowboard_blog/forms.py
+++ snowboard_blog/forms.py
@@ -14,3 +14,26 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        '''
+forms.py
+
+This module contains Django forms used in the application.
+
+- CommentForm: A form used for submitting comments.
+'''
+
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+        labels = {
+            'body': ''
+        }
+        widgets = {
+            'body': forms.Textarea(attrs={'placeholder': 'Write your opinion ', 'required': False}),
+        }
+
