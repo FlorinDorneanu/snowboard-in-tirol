df5afc199d8fcf88f9c43fbf468e4436e7668a3e
FlorinDorneanu <>
2023-08-08 09:54:07 +0000

    Add code comments

--- snowboard_in_tirol/urls.py
+++ snowboard_in_tirol/urls.py
@@ -1,21 +1,8 @@
-"""snowboard_in_tirol URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# * Import the necessary module from Django *
 from django.contrib import admin
 from django.urls import path, include
 
+# * Define urls patterns *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("snowboard_blog.urls"), name="snowboard_blog-urls"),
