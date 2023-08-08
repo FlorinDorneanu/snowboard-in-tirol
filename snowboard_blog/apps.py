638463e6f6a6d45c01cff3eb6561bf556ce19985
FlorinDorneanu <>
2023-08-08 09:43:35 +0000

    Add code comments

--- snowboard_blog/apps.py
+++ snowboard_blog/apps.py
@@ -1,6 +1,8 @@
+# * Import the necessary module from Django *
 from django.apps import AppConfig
 
 
+# * Define configuration class for snowboard blog app *
 class SnowboardBlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'snowboard_blog'
